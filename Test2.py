f03a9c016a7c8914ff2bbe8e511aecca30bd9f02
KatieFitzgerald <C14391781@dit.ie>
2016-03-21 12:25:23 +0000

    Introducing classes

--- Test2.py
+++ Test2.py
@@ -1,27 +1,32 @@
 from cgitb import text
 from tkinter import *
 from tkinter import ttk
+class MyDesign():
+    def __init__(self):
 
-window = Tk()
+        self.window = Tk()
 
-window.title('PipBoy')
+        self.window.title('PipBoy')
 
-radioB = Button(window, text ="RADIO", width=6, bg="black", fg="green")
-radioB.pack(side=TOP)
+        radioB = Button(self.window, text ="RADIO", width=6, bg="black", fg="green")
+        radioB.pack(side=TOP)
 
-mapB = Button(window, text ="MAP", width=6, bg="black", fg="green")
-mapB.pack(side=TOP)
+        mapB = Button(self.window, text ="MAP", width=6, bg="black", fg="green")
+        mapB.pack(side=TOP)
 
-dataB = Button(window, text ="DATA", width=6, bg="black", fg="green")
-dataB.pack(side=TOP)
+        dataB = Button(self.window, text ="DATA", width=6, bg="black", fg="green")
+        dataB.pack(side=TOP)
 
-invB = Button(window, text="INV", width=6, bg="black", fg="green")
-invB.pack(side=TOP)
+        invB = Button(self.window, text="INV", width=6, bg="black", fg="green")
+        invB.pack(side=TOP)
 
-statsB = Button(window, text ="STATS", width=6, bg="black", fg="green")
-statsB.pack(side=TOP)
+        statsB = Button(self.window, text ="STATS", width=6, bg="black", fg="green")
+        statsB.pack(side=TOP)
 
-label = Label(window, text = "Example text")
-label.pack(padx = 200, pady = 200)
+        label = Label(self.window, text = "Example text")
+        label.pack(padx = 200, pady = 200)
 
-window.mainloop()
+        self.window.mainloop()
+
+design = MyDesign()
+design.root.mainloop()
