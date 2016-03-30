4eb752e9b0d3e9ada298b44a06fe1e54516645a2
KatieFitzgerald <C14391781@dit.ie>
2016-03-30 14:30:43 +0100

    Creating stats

--- Test2.py
+++ Test2.py
@@ -1,5 +1,13 @@
+import matplotlib
+import numpy
+
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
 import tkinter as tk
+import tkinter as ttk
 from PIL import Image, ImageTk
+from pylab import *
 
 PIP_FONT = ("Verdana", 12)
 photo = "mrPip.gif"
@@ -10,8 +18,10 @@ class SetUp(tk.Tk):  #inheriting
     def __init__(self, *args, **kwargs):  #method, initialisng
 
         tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self) #container for holding everything
 
+        tk.Tk.wm_iconbitmap(self, default="favicon.ico")
+
+        container = tk.Frame(self) #container for holding everything
         container.pack(side = "top", fill = "both", expand = True)
         container.grid_rowconfigure(0, weight = 1)
         container.grid_columnconfigure(0, weight = 1)
@@ -207,8 +217,18 @@ class StatsPage(tk.Frame):
         stats = tk.Button(self, text ="STATS", bg="black", fg="green", width = 10)
         stats.grid(row = 0, column=5)
 
-        label = tk.Label(self, text = "stats functionality", bg = "black", fg = "white")
-        label.grid(row = 1, column = 1)
+        f = Figure(figsize = (4,2), dpi = 100)
+        a = f.add_subplot(111) #onexone chart number 1
+
+        data = (1, 2, 3, 4, 5)
+        ind = numpy.arange(5) #the x plots
+        width = .5
+
+        barh = a.bar(ind, data, width)
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().grid(row = 1, column = 1)
 
 app = SetUp()
 app.mainloop()
