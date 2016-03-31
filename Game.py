492713edaea98b2ea9b56a5b39ca82f58be449fd
Cm996 <mccabecillian@gmail.com>
2016-03-31 14:23:35 +0100

    Adding game display window

--- Game.py
+++ Game.py
@@ -7,3 +7,14 @@ black = (0, 0, 0)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
+
+gameDisplay = pygame.display.set_mode((800, 600))
+gameDisplay.fill(black)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.quit:
+            pygame.quit()
+            quit()
+
+    pygame.display.update()
