f2a61cf666b9cbbc3a93fa63688897bb84a59751
Cm996 <mccabecillian@gmail.com>
2016-04-02 01:23:41 +0100

    Pause screen working with text

--- Game.py
+++ Game.py
@@ -46,6 +46,28 @@ def message_to_screen(msg, color, y_displace=0, size= "smallFont"):
     gameDisplay.blit(textSurface, textRect)
 
 
+def pause():
+    paused = True
+    message_to_screen("Paused", green, -100, size="largeFont")
+    message_to_screen("Press C to continue playing or Q to quit", green, 25)
+    pygame.display.update()
+    while paused:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_c:
+                        paused = False
+
+                    elif event.key == pygame.K_q:
+                        pygame.quit()
+                        quit()
+
+        clock.tick(5)
+
+
 def game_intro():  # Function for game introduction screen
 
     intro = True
@@ -113,6 +135,9 @@ def gameLoop():
                 elif event.key == pygame.K_DOWN:
                     pass
 
+                elif event.key == pygame.K_p:
+                    pause()
+
         gameDisplay.fill(black)
         pygame.display.update()
         clock.tick(FPS)
@@ -121,25 +146,3 @@ def gameLoop():
     quit()
 
 gameLoop()
-
-
-def pause():
-    paused = True
-    message_to_screen("Paused", green, -100, size="large")
-    message_to_screen("Press C to continue playing or Q to quit", green, 25)
-    pygame.display.update()
-    while paused:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
-
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_c:
-                        paused = False
-
-                    elif event.key == pygame.K_q:
-                        pygame.quit()
-                        quit()
-
-        clock.tick(5)
