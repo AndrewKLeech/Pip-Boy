7827df810ad02eeacdd3dc46f9b095073f81c484
Cm996 <mccabecillian@gmail.com>
2016-04-06 20:45:06 +0100

    Editing controls page

--- Game.py
+++ Game.py
@@ -164,7 +164,7 @@ def game_controls():  # Function for controls screen
         message_to_screen("Controls!", green, -100, size="largeFont")
         message_to_screen("Left and right arrow keys to move the tank!", green, 10, size="smallFont")
         message_to_screen("Up and down arrow keys to move the tank's turret!", green, 40, size="smallFont")
-        message_to_screen("Controls!", green, 70, size="smallFont")
+        message_to_screen("A  and D keys change the turret's power!", green, 70, size="smallFont")
         message_to_screen("P to pause the game!", green, 100, size="smallFont")
 
         # Buttons
