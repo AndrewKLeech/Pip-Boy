2f904ab442df5b94c83a56b800690b82ef93e5b3
AndrewKLeech <andrewkennethleech@gmail.com>
2016-04-17 15:21:12 +0100

    run with less code, add get location

--- localMap.py
+++ localMap.py
@@ -1,86 +1,27 @@
-import urllib.request
-import urllib.parse
-import urllib.error
 import io
-from math import log, exp, tan, atan, pi, ceil
+import urllib.request
 from PIL import Image
 
-EARTH_RADIUS = 6378137
-EQUATOR_CIRCUMFERENCE = 2 * pi * EARTH_RADIUS
-INITIAL_RESOLUTION = EQUATOR_CIRCUMFERENCE / 256.0
-ORIGIN_SHIFT = EQUATOR_CIRCUMFERENCE / 2.0
+
+def get_location(query):
 
 
-def latlontopixels(lat, lon, zoom):
-    mx = (lon * ORIGIN_SHIFT) / 180.0
-    my = log(tan((90 + lat) * pi / 360.0)) / (pi / 180.0)
-    my = (my * ORIGIN_SHIFT) / 180.0
-    res = INITIAL_RESOLUTION / (2 ** zoom)
-    px = (mx + ORIGIN_SHIFT) / res
-    py = (my + ORIGIN_SHIFT) / res
-    return px, py
+    params = { }
+    params[ 'sensor' ] = "false"
+    params[ 'address' ] = query
+
+    params = urllib.parse.urlencode( params )
+    print("https://maps.googleapis.com/maps/api/geocode/json?%s" % params)
+
+    f = urllib.request.urlopen("https://maps.googleapis.com/maps/api/geocode/json?%s" % params)
 
 
-def pixelstolatlon(px, py, zoom):
-    res = INITIAL_RESOLUTION / (2 ** zoom)
-    mx = px * res - ORIGIN_SHIFT
-    my = py * res - ORIGIN_SHIFT
-    lat = (my / ORIGIN_SHIFT) * 180.0
-    lat = 180 / pi * (2 * atan(exp(lat * pi / 180.0)) - pi / 2.0)
-    lon = (mx / ORIGIN_SHIFT) * 180.0
-    return lat, lon
+def get_map():
 
+    url = "https://maps.googleapis.com/maps/api/staticmap?center=53.3766353,-6.3887243&size=500x500&zoom=15&sensor=false"
+    buffer = io.BytesIO(urllib.request.urlopen(url).read())
+    image = Image.open(buffer)
+    image.show()
 
-############################################
-
-# a neighbourhood in Lajeado, Brazil:
-
-upperleft = '-29.44,-52.0'
-lowerright = '-29.45,-51.98'
-
-zoom = 18  # be careful not to get too many images!
-
-############################################
-
-ullat, ullon = map(float, upperleft.split(','))
-lrlat, lrlon = map(float, lowerright.split(','))
-
-# Set some important parameters
-scale = 1
-maxsize = 640
-
-# convert all these coordinates to pixels
-ulx, uly = latlontopixels(ullat, ullon, zoom)
-lrx, lry = latlontopixels(lrlat, lrlon, zoom)
-
-# calculate total pixel dimensions of final image
-dx, dy = lrx - ulx, uly - lry
-
-# calculate rows and columns
-cols, rows = int(ceil(dx / maxsize)), int(ceil(dy / maxsize))
-
-# calculate pixel dimensions of each small image
-bottom = 120
-largura = int(ceil(dx / cols))
-altura = int(ceil(dy / rows))
-alturaplus = altura + bottom
-
-final = Image.new("RGB", (int(dx), int(dy)))
-for x in range(cols):
-    for y in range(rows):
-        dxn = largura * (0.5 + x)
-        dyn = altura * (0.5 + y)
-        latn, lonn = pixelstolatlon(ulx + dxn, uly - dyn - bottom / 2, zoom)
-        position = ','.join((str(latn), str(lonn)))
-        print(x, y, position)
-        urlparams = urllib.parse.urlencode({'center': position,
-                                      'zoom': str(zoom),
-                                      'size': '%dx%d' % (largura, alturaplus),
-                                      'maptype': 'satellite',
-                                      'sensor': 'false',
-                                      'scale': scale})
-        url = 'http://maps.google.com/maps/api/staticmap?' + urlparams
-        f=urllib.request.urlopen(url)
-        im=Image.open(io.BytesIO(f.read()))
-        final.paste(im, (int(x*largura), int(y*altura)))
-final.show()
+get_location("14 cherry ave, dublin 15")
+get_map()
