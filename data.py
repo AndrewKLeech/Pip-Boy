46b31570d2ef1676caf182424fa8b9d179a0f4ee
AndrewKLeech <andrewkennethleech@gmail.com>
2016-04-18 10:39:52 +0100

    Get name and tweet of 5 newest tweets

--- data.py
+++ data.py
@@ -2,8 +2,8 @@ from twitter import *
 
 #Put in token, token_key, con_secret, con_secret_key
 t = Twitter(
-    auth=OAuth('', '',
-               '', ''))
+    auth=OAuth('705153959368007680-F5OUf8pvmOlXku1b7gpJPSAToqzV4Fb', 'bEGLkUJBziLc17EuKLTAMio8ChmFxP9aHYADwRXnxDsoC',
+               'gYDgR8lcTGcVZS9ucuEIYsMuj', '1dwHsLDN2go3aleQ8Q2vcKRfLETc51ipsP8310ayizL2p3Ycii'))
 
 #get user status
 print(t.statuses.user_timeline(screen_name="AndrewKLeech"))
