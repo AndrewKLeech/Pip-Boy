5808b7e8a84b66f11f93011744ffdca836c30702
AndrewKLeech <andrewkennethleech@gmail.com>
2016-04-18 11:27:49 +0100

    Display tweets in window

--- data.py
+++ data.py
@@ -1,18 +1,20 @@
 from twitter import *
+from tkinter import *
 
 
 def showTweets(x, num):
     # display a number of new tweets and usernames
     for i in range(0, num):
-        print(x[i]['user']['screen_name'])
-        print(x[i]['text'])
-
+        line1 = (x[i]['user']['screen_name'])
+        line2 = (x[i]['text'])
+        w = Label(master, text=line1 + "\n" + line2)
+        w.pack()
 
 def getTweets():
     # Put in token, token_key, con_secret, con_secret_key
     t = Twitter(
-        auth=OAuth('', '',
-                   '', ''))
+        auth=OAuth('705153959368007680-F5OUf8pvmOlXku1b7gpJPSAToqzV4Fb', 'bEGLkUJBziLc17EuKLTAMio8ChmFxP9aHYADwRXnxDsoC',
+                   'gYDgR8lcTGcVZS9ucuEIYsMuj', '1dwHsLDN2go3aleQ8Q2vcKRfLETc51ipsP8310ayizL2p3Ycii'))
 
     x = t.statuses.home_timeline(screen_name="AndrewKLeech")
     return x
@@ -20,4 +22,10 @@ def getTweets():
 
 numberOfTweets = 5
 
+
+
+master = Tk()
 showTweets(getTweets(), numberOfTweets)
+
+
+mainloop()
