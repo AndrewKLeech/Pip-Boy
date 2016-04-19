6a404a7b9fc90954812c8b2e1a8f97d758b4b44e
AndrewKLeech <andrewkennethleech@gmail.com>
2016-04-19 13:05:15 +0100

    remove useless code

--- localMap.py
+++ localMap.py
@@ -6,19 +6,20 @@ import socket
 import simplejson
 from tkinter import *
 
+#url used for google geocodeing api
 googleGeocodeUrl = 'http://maps.googleapis.com/maps/api/geocode/json?'
 
 
 def get_map(lat,lng):
     latString = str(lat)
     lngString = str(lng)
-    print(lat)
+    #Map url from google maps, has marker and colors included
     url = ("https://maps.googleapis.com/maps/api/staticmap?center="+latString+","+lngString+"&size=500x500&zoom=16&style=feature:road.local%7Celement:geometry%7Ccolor:0x00ff00%7Cweight:1%7Cvisibility:on&style=feature:landscape%7Celement:geometry.fill%7Ccolor:0x000000%7Cvisibility:on&style=feature:landscape%7Celement:geometry.fill%7Ccolor:0x000000%7Cvisibility:on&style=feature:administrative%7Celement:labels%7Cweight:3.9%7Cvisibility:on%7Cinverse_lightness:true&style=feature:poi%7Cvisibility:simplified&markers=color:blue%7Clabel:H%7C"+latString+","+lngString+"&markers=size:tiny%7Ccolor:green%7CDelta+Junction,AK\
 &sensor=false")
     buffer = BytesIO(urllib.request.urlopen(url).read())
     pil_image = PIL.Image.open(buffer)
     tk_image = ImageTk.PhotoImage(pil_image)
-    # put the image on a typical widget
+    # put the image in program
     label = Label(master, image=tk_image)
     label.pack(padx=5, pady=5)
     master.mainloop()
@@ -53,7 +54,7 @@ def get_coordinates(from_sensor=False):
 master = Tk()
 
 
-master.title("Tkinter Entry Widget")
+master.title("Map")
 master.geometry('500x500')
 
 master["padx"] = 40
